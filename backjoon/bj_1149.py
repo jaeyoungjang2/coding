0044dca57c0cfa7a7ec6de320e966584f6191b1b
jaeyoungjang2 <lion85488@gmail.com>
2021-01-14 22:26:19 -0800

    Update bj_1149.py

--- backjoon/bj_1149.py
+++ backjoon/bj_1149.py
@@ -1,11 +1,7 @@
-#n = 3
-#26 40 83
-#49 60 57
-#13 89 99
 import sys
 n = int(input())
 res = list(map(int,sys.stdin.readline().rstrip().split()))
-print(res)
+
 for _ in range(n-1):
     lst = list(map(int,sys.stdin.readline().rstrip().split()))
     res0 = min(res[1],res[2])+lst[0]
@@ -14,10 +10,5 @@ for _ in range(n-1):
     res[0] = res0
     res[1] = res1
     res[2] = res2
-    print(res)
 
 print(min(res))
-    
-
-
-        
