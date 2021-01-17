1c6c5b648deb13c0869b54b44032d84cf95a9c01
jaeyoungjang2 <lion85488@gmail.com>
2021-01-16 16:30:39 -0800

    Update bj_1932.py

--- bj_1932.py
+++ bj_1932.py
@@ -1,21 +1,13 @@
-import sys 
-
+import sys
 n = int(input())
-lst = []
-for i in range(n):
-    temp = list(map(int,sys.stdin.readline().rstrip().split()))
-    lst.append(temp)
-    if i == 0:
-        continue
-    else:
-        for j in range(0,i+1):
-            if j == 0:
-                lst[i][0] += lst[i-1][0]
-            elif j == i:
-                lst[i][j] += lst[i-1][j-1]
-            else:
-                lst[i][j] += max(lst[i-1][j-1],lst[i-1][j])
+lst = list(map(int,sys.stdin.readline().rstrip().split()))
+d = [0] * n
 
-print(max(lst[-1]))
-
-
+for idx, i in enumerate(lst):
+    
+    for j in range(idx):
+        if d[j] > d[idx] and lst[j] < lst[idx]:
+            d[idx] = d[j]
+    d[idx] += i
+        
+print(max(d))
