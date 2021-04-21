ea26413de2cca14569b5db4e05a35a4517cfef1a
jaeyoungjang2 <lion85488@gmail.com>
2021-04-21 22:08:55 +0900

    Create 9095_2.py

---
+++ 9095_2.py
@@ -0,0 +1,17 @@
+def cal(sum, goal):
+
+    if sum > goal:
+        return 0
+    elif sum == goal:
+        return 1
+    now = 0
+    print(now)
+    for i in range(1, 4):
+        now += cal(sum + i, goal)
+    return now
+
+
+count = int(input())
+for i in range(count):
+    target = int(input())
+    print(cal(0, target))
