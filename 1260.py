1a1b86bc02a7d5add2a6103488e8efe547d9fdad
jaeyoungjang2 <lion85488@gmail.com>
2021-05-09 22:05:50 +0900

    Update 1260.py

--- 1260.py
+++ 1260.py
@@ -1,26 +1,58 @@
-e, v, start = map(int, input().split())
+import sys
+N, M, V = list(map(int, sys.stdin.readline().split()))
 
-graph = [[] for i in range(e+1)]
-visit = [False for i in range(e+1)]
 
-for i in range(v):
-    a, b = map(int, input().split())
-    graph[a] = b
-    graph[b] = a
+lst = [[] for _ in range(N+1)]
+bfslst = [[] for _ in range(N+1)]
+stackLst = [V]
+queueLst = [V]
 
-def go(visit, target, depth,DFS):
-    if depth >= e:
-        DFS_res = True
+for _ in range(M):
+    a, b = list(map(int, sys.stdin.readline().split()))
+    if a not in lst[b]:
+        lst[b].append(a)
+        bfslst[b].append(a)
+    if b not in lst[a]:
+        lst[a].append(b)
+        bfslst[a].append(b)
+
+
+def dfs(stack, order, lst):
+    if len(stack) == 0:
+        print(' '.join(map(str, order)))
         return
-    for i in graph[target]:
-        if visit[i] == False:
-            visit[i] = True
-            go(visit, i, depth+1)
+    if len(lst[stack[-1]]) == 0:
+        stack.pop()
+        dfs(stack, order, lst)
+        return
+    target = lst[stack[-1]].pop(0)
+    if target not in order:
+        stack.append(target)
+        order.append(target)
+        dfs(stack, order, lst)
+    elif target in order:
+        if len(lst[stack[-1]]) == 0:
+            stack.pop()
+        dfs(stack, order, lst)
 
 
-visit[0] = True
-visit[1] = True
-DFS = [1]
-DFS_res = False
-BFS = [1]
-go(visit,1,0,DFS))
+def bfs(queue, order, lst):
+    if len(queue) == 0:
+        print(' '.join(map(str, order)))
+        return
+    if len(lst[queue[-1]]) == 0:
+        queue.pop(0)
+        bfs(queue, order, lst)
+        return
+    targetLst = lst[queue.pop(0)]
+    for i in targetLst:
+        if i not in order:
+            queue.append(i)
+            order.append(i)
+    bfs(queue, order, lst)
+
+
+dfsOrder = [V]
+bfsOrder = [V]
+dfs(stackLst, dfsOrder, lst)
+bfs(queueLst, bfsOrder, bfslst)
